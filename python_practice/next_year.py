64a0167575f7696749a992cc687db54bf23348eb
RidhoPH <ridho.pratomo2305@gmail.com>
2025-01-22 01:40:10 +0000

    Assigment Submitted Ridho

---
+++ python_practice/next_year.py
@@ -0,0 +1,17 @@
+#greetings
+print("Hello, world!")
+
+#ask user's name
+name = input("Enter your name: ")
+
+#ask user's age
+age = float(input("Enter your age: "))
+
+#tell the user's name & age
+print(f"Hello {name}! you are {age} years old. ")
+
+#tell the user's age next year
+print(f"Next year you will be {age+1} years old. ")
+
+
+      
